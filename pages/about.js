eef4506b6e2a468fd0dde0dfcdd6ca79d33d11cf
Michelangelo Serpico <mise@OSX-MISE-N-IT.local>
2023-03-06 17:17:57 +0100

    var env

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,20 @@
-export default function About() {
+export default function About({welcome}) {
     return (
         <>
             <h1>About NextJS</h1>
-            <p>text text</p>
+            <p>{welcome}</p>
         </>
     )
+}
+
+
+// Server side execution 
+export async function getStaticProps({ params }) {
+    let envvar = process.env.MISE_VAR 
+    console.log('====> ENV VAR:', envvar)
+    return {
+        props: {
+            welcome: envvar
+        }
+    }
 }
